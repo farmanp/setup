2cec753531e81f83d6469419ce94ff1964693a51
Farman Pirzada <farman.pirz@gmail.com>
2021-03-13 22:28:55 -0700

    Ready for merge

--- mac_setup.sh
+++ mac_setup.sh
@@ -1,10 +1,10 @@
 # Overview of this script is running an automated installation workflow that goes step by step below:
-# 1. Github setup 
-# 2. Install XCode 
+# 1. Github setup
+# 2. Install XCode
 # 3. Install Homebrew
-# 4. Install Development tools (programming languages, libraries, etc) 
+# 4. Install Development tools (programming languages, libraries, etc)
 # 5. Setting up Mac settings
-# Done! 
+# Done!
 
 developer_utilities_setup() {
 	echo "Installing tree, wget, trash, mackup, and node..."
@@ -14,6 +14,18 @@ developer_utilities_setup() {
 	# brew install svn
 	brew install mackup
 	brew install node
+}
+
+browser_install_setup() {
+	browsers=(
+		firefox
+		google-chrome
+  )
+
+	echo "installing appropriate browsers with Cask..."
+	brew install --cask --appdir="/Applications" ${browsers[@]}
+	brew cask cleanup
+	brew cleanup
 }
 
 git_setup() {
@@ -32,14 +44,16 @@ github_setup() {
 
 	echo "Please add this public key to Github \n"
 	echo "https://github.com/account/ssh \n"
+	echo "Opening the link now \n"
+	sleep 2
+	open "https://github.com/account/ssh"a
 	read -p "Press [Enter] key after this..."
-	# Need to write prompts for this section 
+	# Need to write prompts for this section
 	# echo "Git config"
 
 	# git config --global user.name "Farman Pirzada"
-	# git config --global user.email farman.pirz@gmail.com	
+	# git config --global user.email farman.pirz@gmail.com
 }
-
 
 xcode_installer_setup() {
 	echo "Installing xcode"
@@ -62,8 +76,7 @@ homebrew_setup() {
 	brew cleanup
 
 	echo "Installing homebrew cask"
-	brew install caskroom/cask/brew-cask
-}
+	brew install cask
 
 #@TODO install our custom fonts and stuff
 
@@ -78,7 +91,6 @@ go_setup() {
 	echo "Installing GoLang"
 	brew install go
 }
-
 
 java_setup() {
 	echo "Installing Java 1.8"
@@ -101,8 +113,8 @@ nvm_setup() {
 }
 
 rbenv_setup() {
-	echo "Installing rbenv" 
-	brew install rbenv 
+	echo "Installing rbenv"
+	brew install rbenv
 }
 
 iterm2_setup() {
@@ -163,181 +175,15 @@ applications_setup() {
 # echo "installing apps with Cask..."
 # brew cask install --appdir="/Applications" ${apps[@]}
 # brew cask alfred link
-# brew cask cleanup
-# brew cleanup
 
-# echo "Restoring setup from Mackup..."
-#mackup restore @TODO uncomment
-
-macbook_settings() {
-	echo "Setting some Mac settings..."
-
-	#"Disabling system-wide resume"
-	defaults write NSGlobalDomain NSQuitAlwaysKeepsWindows -bool false
-
-	#"Disabling automatic termination of inactive apps"
-	defaults write NSGlobalDomain NSDisableAutomaticTermination -bool true
-
-	#"Allow text selection in Quick Look"
-	defaults write com.apple.finder QLEnableTextSelection -bool TRUE
-
-	#"Disabling OS X Gate Keeper"
-	#"(You'll be able to install any app you want from here on, not just Mac App Store apps)"
-	sudo spctl --master-disable
-	sudo defaults write /var/db/SystemPolicy-prefs.plist enabled -string no
-	defaults write com.apple.LaunchServices LSQuarantine -bool false
-
-	#"Expanding the save panel by default"
-	defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
-	defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
-	defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
-
-	#"Automatically quit printer app once the print jobs complete"
-	defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
-
-	#"Saving to disk (not to iCloud) by default"
-	defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
-
-	#"Check for software updates daily, not just once per week"
-	defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
-
-	#"Disable smart quotes and smart dashes as they are annoying when typing code"
-	defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
-	defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
-
-	#"Enabling full keyboard access for all controls (e.g. enable Tab in modal dialogs)"
-	defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
-
-	#"Disabling press-and-hold for keys in favor of a key repeat"
-	defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
-
-	#"Setting trackpad & mouse speed to a reasonable number"
-	defaults write -g com.apple.trackpad.scaling 2
-	defaults write -g com.apple.mouse.scaling 2.5
-
-	#"Enabling subpixel font rendering on non-Apple LCDs"
-	defaults write NSGlobalDomain AppleFontSmoothing -int 2
-
-	#"Showing icons for hard drives, servers, and removable media on the desktop"
-	defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
-
-	#"Showing all filename extensions in Finder by default"
-	defaults write NSGlobalDomain AppleShowAllExtensions -bool true
-
-	#"Disabling the warning when changing a file extension"
-	defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
-
-	#"Use column view in all Finder windows by default"
-	defaults write com.apple.finder FXPreferredViewStyle Clmv
-
-	#"Avoiding the creation of .DS_Store files on network volumes"
-	defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
-
-	#"Enabling snap-to-grid for icons on the desktop and in other icon views"
-	/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
-	/usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
-	/usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
-
-	#"Setting the icon size of Dock items to 36 pixels for optimal size/screen-realestate"
-	defaults write com.apple.dock tilesize -int 36
-
-	#"Speeding up Mission Control animations and grouping windows by application"
-	defaults write com.apple.dock expose-animation-duration -float 0.1
-	defaults write com.apple.dock "expose-group-by-app" -bool true
-
-	#"Setting Dock to auto-hide and removing the auto-hiding delay"
-	defaults write com.apple.dock autohide -bool true
-	defaults write com.apple.dock autohide-delay -float 0
-	defaults write com.apple.dock autohide-time-modifier -float 0
-
-	#"Setting email addresses to copy as 'foo@example.com' instead of 'Foo Bar <foo@example.com>' in Mail.app"
-	defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
-
-	#"Enabling UTF-8 ONLY in Terminal.app and setting the Pro theme by default"
-	defaults write com.apple.terminal StringEncodings -array 4
-	defaults write com.apple.Terminal "Default Window Settings" -string "Pro"
-	defaults write com.apple.Terminal "Startup Window Settings" -string "Pro"
-
-	#"Preventing Time Machine from prompting to use new hard drives as backup volume"
-	defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
-
-	#"Disable the sudden motion sensor as its not useful for SSDs"
-	sudo pmset -a sms 0
-
-	#"Speeding up wake from sleep to 24 hours from an hour"
-	# http://www.cultofmac.com/221392/quick-hack-speeds-up-retina-macbooks-wake-from-sleep-os-x-tips/
-	sudo pmset -a standbydelay 86400
-
-	#"Disable annoying backswipe in Chrome"
-	defaults write com.google.Chrome AppleEnableSwipeNavigateWithScrolls -bool false
-
-	#"Setting screenshots location to ~/Desktop"
-	defaults write com.apple.screencapture location -string "$HOME/Desktop"
-
-	#"Setting screenshot format to PNG"
-	defaults write com.apple.screencapture type -string "png"
-
-	#"Hiding Safari's bookmarks bar by default"
-	defaults write com.apple.Safari ShowFavoritesBar -bool false
-
-	#"Hiding Safari's sidebar in Top Sites"
-	defaults write com.apple.Safari ShowSidebarInTopSites -bool false
-
-	#"Disabling Safari's thumbnail cache for History and Top Sites"
-	defaults write com.apple.Safari DebugSnapshotsUpdatePolicy -int 2
-
-	#"Enabling Safari's debug menu"
-	defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
-
-	#"Making Safari's search banners default to Contains instead of Starts With"
-	defaults write com.apple.Safari FindOnPageMatchesWordStartsOnly -bool false
-
-	#"Removing useless icons from Safari's bookmarks bar"
-	defaults write com.apple.Safari ProxiesInBookmarksBar "()"
-
-	#"Allow hitting the Backspace key to go to the previous page in history"
-	defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2BackspaceKeyNavigationEnabled -bool true
-
-	#"Enabling the Develop menu and the Web Inspector in Safari"
-	defaults write com.apple.Safari IncludeDevelopMenu -bool true
-	defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
-	defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
-
-	#"Adding a context menu item for showing the Web Inspector in web views"
-	defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
-
-	#"Use `~/Downloads/Incomplete` to store incomplete downloads"
-	defaults write org.m0k.transmission UseIncompleteDownloadFolder -bool true
-	defaults write org.m0k.transmission IncompleteDownloadFolder -string "${HOME}/Downloads/Incomplete"
-
-	#"Don't prompt for confirmation before downloading"
-	defaults write org.m0k.transmission DownloadAsk -bool false
-
-	#"Trash original torrent files"
-	defaults write org.m0k.transmission DeleteOriginalTorrent -bool true
-
-	#"Hide the donate message"
-	defaults write org.m0k.transmission WarningDonate -bool false
-
-	#"Hide the legal disclaimer"
-	defaults write org.m0k.transmission WarningLegal -bool false
-
-	#"Disable 'natural' (Lion-style) scrolling"
-	defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
-
-	# Don’t automatically rearrange Spaces based on most recent use
-	defaults write com.apple.dock mru-spaces -bool false
-
-	killall Finder
-}
-
+homebrew_setup
 developer_utilities_setup
+browser_install_setup
 git_setup
 github_setup
 xcode_installer_setup
-homebrew_setup
-# DEVELOPMENT ENVIRONMENT SETUP 
-echo "Installing appropriate development tools environment" 
+# DEVELOPMENT ENVIRONMENT SETUP
+echo "Installing appropriate development tools environment"
 go_setup
 java_setup
 net_setup
@@ -345,5 +191,4 @@ nvm_setup
 rbenv_setup
 iterm2_setup
 zsh_setup
-macbook_settings
 echo "Done!"
